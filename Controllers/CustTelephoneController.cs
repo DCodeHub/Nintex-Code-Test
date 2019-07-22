33007ebef7962948092b10a0831df0f9d274b90e
DCodeHub <32890071+DCodeHub@users.noreply.github.com>
2019-07-23 09:45:54 +1000

    Add files via upload

---
+++ Controllers/CustTelephoneController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APIForCustomerTelephone.Model;
+
+namespace APIForCustomerTelephone.Controllers
+{
+    public class CustTelephoneController : ApiController
+    {
+        private CustomerPhoneEntities dbContext = new CustomerPhoneEntities();
+
+    
+        [HttpGet]
+        public List<CustomerPhone> GetAllTelephoneNumbers()
+        {
+            return dbContext.CustomerPhones.ToList();
+        }
+
+
+        [HttpGet]
+
+        public string[] GetTelephoneNumbersByCustomer(int customerID)
+        {
+            return dbContext.CustomerPhones.Select(c => c.customerPhone).ToArray();
+        }
+
+
+        [HttpPost]
+
+        public void AddTelephoneNumbers(int customerID, string phoneNumber)
+        {
+            var p = new CustomerPhone { customerPhone = phoneNumber, customerId = customerID };
+            dbContext.CustomerPhones.Add(p);
+            dbContext.SaveChanges();
+
+        }
+
+        /// <summary>
+        /// Activate Telephone number for a customer
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public void ActivateTelephoneNumbers(int customerID, string phoneNumber, string status)
+        {
+            using (var db = new CustomerPhoneEntities())
+            {
+                var result = db.CustomerPhones.SingleOrDefault(b => b.customerId == customerID);
+                if (result != null)
+                {
+                    try
+                    {
+                        db.CustomerPhones.Attach(result);
+                        result.customerId = customerID;
+                        result.phoneStaus = status;
+                        db.Entry(result).State = System.Data.Entity.EntityState.Modified; 
+                        db.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+            }
+            //var p = new CustomerPhone { customerPhone = phoneNumber, customerId = customerID, phoneStaus = status };
+            //dbContext.CustomerPhones.Attach(p);
+            //dbContext.Entry(p).State = System.Data.Entity.EntityState.Modified;
+            //dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Controllers/CustomerPhoneController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using APIForCustomerTelephone.Model;
+
+namespace APIForCustomerTelephone.Controllers
+{
+    public class CustomerPhoneController : ApiController
+    {
+        private CustomerPhoneEntities db = new CustomerPhoneEntities();
+
+        // GET: api/CustomerPhone
+        public IQueryable<CustomerPhone> GetCustomerPhones()
+        {
+            return db.CustomerPhones;
+        }
+
+        // GET: api/CustomerPhone/5
+        [ResponseType(typeof(CustomerPhone))]
+        public IHttpActionResult GetCustomerPhone(int id)
+        {
+            CustomerPhone customerPhone = db.CustomerPhones.Find(id);
+            if (customerPhone == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customerPhone);
+        }
+
+        // PUT: api/CustomerPhone/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCustomerPhone(int id, CustomerPhone customerPhone)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != customerPhone.phoneId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(customerPhone).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerPhoneExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CustomerPhone
+        [ResponseType(typeof(CustomerPhone))]
+        public IHttpActionResult PostCustomerPhone(CustomerPhone customerPhone)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CustomerPhones.Add(customerPhone);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = customerPhone.phoneId }, customerPhone);
+        }
+
+        // DELETE: api/CustomerPhone/5
+        [ResponseType(typeof(CustomerPhone))]
+        public IHttpActionResult DeleteCustomerPhone(int id)
+        {
+            CustomerPhone customerPhone = db.CustomerPhones.Find(id);
+            if (customerPhone == null)
+            {
+                return NotFound();
+            }
+
+            db.CustomerPhones.Remove(customerPhone);
+            db.SaveChanges();
+
+            return Ok(customerPhone);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CustomerPhoneExists(int id)
+        {
+            return db.CustomerPhones.Count(e => e.phoneId == id) > 0;
+        }
+    }
+}
